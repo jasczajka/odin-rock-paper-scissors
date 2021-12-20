1e239334bd533251f424ae6fe2b73577290b4564
jasczajka <czajka.jan12@gmail.com>
2021-12-20 21:11:49 +0100

    create capitalize function and getPlayerSelection function

--- myScript.js
+++ myScript.js
@@ -23,14 +23,48 @@ function computerPlay()
 
 }
 computerPlay();
-/* 
-function gameRound(playerSelection, computerSelection)
+function capitalize(s)
+{
+    return s && s[0].toUpperCase() + s.slice(1);
+}
+
+function getPlayerSelection()
+{
+    //initialize the loop
+    let keepGoing = true;
+    //while the loop is initialized
+    while(keepGoing)
+    {
+    //ask the user for input
+        let choice = window.prompt("Choose Rock, Paper or Scissors", "Rock / Paper / Scissors");
+    //capitalize the input
+        choiceCapitalized = capitalize(choice);
+    //if the input is valid
+        if (choiceCapitalized === "Rock" || choiceCapitalized === "Paper" || choiceCapitalized === "Scissors")
+        {
+    //break the loop and return the value
+            let keepGoing = false;
+            return choiceCapitalized;
+        }
+    }
+}
+console.log(getPlayerSelection());
+
+/* function gameRound(playerSelection, computerSelection)
 {
     //play a random round of rock paper scissors and return the winner
     //player selection should be case insensitive
-}
+    //fix capitalization in playerSelectionvariable
+    let playerSelectionCamelCase = playerSelection.capitalize();
+    //check if player's input is valid
+    if (playerSelectionCamelCase != "Rock" || playerSelectionCamelCase != "Paper" || playerSelectionCamelCase != "Scissors")
+    {
 
-game()
+    }
+}
+const gameButton = document.getElementById("gameButton");
+gameButton.addEventListener("click",gameRound);
+/* game()
 {
     //5 round game
 }
